84021dd92122e3dca8e2b090009b71a1c7c89ff6
junjungoal <junyamada107@gmail.com>
2020-01-13 13:12:50 -0800

    create reacher-obstacle environment

--- env/reacher/reacher_obstacle.py
+++ env/reacher/reacher_obstacle.py
@@ -1,4 +1,5 @@
 from env.base import BaseEnv
+import numpy as np
 
 
 class ReacherObstacleEnv(BaseEnv):
@@ -6,12 +7,58 @@ class ReacherObstacleEnv(BaseEnv):
 
     def __init__(self):
         super().__init__("reacher_obstacle.xml")
+        self.obstacle_names = ["obstacle1", "obstacle2", "obstacle3",
+                               "obstacle4", "obstacle5", "obstacle6",
+                               "obstacle7", "obstacle8"]
 
     def _reset(self):
         self._set_camera_position(0, [0, -1.0, 1.0])
         self._set_camera_rotation(0, [0, 0, 0])
-        return {}
+        self._reset_obstacles()
+        qpos = np.random.uniform(low=-0.1, high=0.1, size=self.model.nq) + self.sim.data.qpos.ravel()
+        while True:
+            self.goal = np.random.uniform(low=-.3, high=.3, size=2)
+            if np.linalg.norm(self.goal) < 0.2:
+                break
+        qpos[-2:] = self.goal
+        qvel = np.random.uniform(low=-.005, high=.005, size=self.model.nv) + self.sim.data.qvel.ravel()
+        qvel[-2:] = 0
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def _reset_obstacles(self):
+        self._set_pos("obstacle1", np.concatenate([np.random.uniform(0.01, 0.2, 1), np.random.uniform(0.01, 0.2, 1), np.array([0.01])]))
+        self._set_pos("obstacle2", np.concatenate([np.random.uniform(0.01, 0.2, 1), np.random.uniform(0.01, 0.2, 1), np.array([0.01])]))
+        self._set_pos("obstacle3", np.concatenate([np.random.uniform(-0.2, -0.01, 1), np.random.uniform(0.01, 0.2, 1), np.array([0.01])]))
+        self._set_pos("obstacle4", np.concatenate([np.random.uniform(-0.2, -0.01, 1), np.random.uniform(0.01, 0.2, 1), np.array([0.01])]))
+        self._set_pos("obstacle5", np.concatenate([np.random.uniform(0.01, 0.2, 1), np.random.uniform(-0.2, -0.01, 1), np.array([0.01])]))
+        self._set_pos("obstacle6", np.concatenate([np.random.uniform(0.01, 0.2, 1), np.random.uniform(-0.2, -0.01, 1), np.array([0.01])]))
+        self._set_pos("obstacle7", np.concatenate([np.random.uniform(-0.2, -0.01, 1), np.random.uniform(-0.2, -0.01, 1), np.array([0.01])]))
+        self._set_pos("obstacle8", np.concatenate([np.random.uniform(-0.2, -0.01, 1), np.random.uniform(-0.2, -0.01, 1), np.array([0.01])]))
+
+    def _get_obstacle_states(self):
+        obstacle_states = []
+        for name in self.obstacle_names:
+            obstacle_states.extend(self._get_pos(name)[:2])
+        return np.array(obstacle_states)
+
+
+    def _get_obs(self):
+        theta = self.sim.data.qpos.flat[:2]
+        return np.concatenate([
+            np.cos(theta),
+            np.sin(theta),
+            self.sim.data.qpos.flat[2:],
+            self.sim.data.qvel.flat[:2],
+            self._get_obstacle_states(),
+            self._get_pos("fingertip") - self._get_pos("target")
+        ])
 
     def _step(self, action):
+        reward_dist = -self._get_distance("fingertip", "target")
+        reward_ctrl = self._ctrl_reward(action)
+        reward = reward_dist + reward_ctrl
         self._do_simulation(action)
-        return {}, 0, False, {}
+        obs = self._get_obs()
+        done = False
+        return obs, reward, done, dict(reward_dist=reward_dist, reward_ctrl=reward_ctrl)
--- env/test_env.py
+++ env/test_env.py
@@ -4,8 +4,7 @@ import gym
 env = gym.make('reacher-obstacle-v0')
 env.reset()
 
-for i in range(1000):
+for i in range(200):
     env.render(mode='human')
     action = env.action_space.sample()
     env.step(action)
-
