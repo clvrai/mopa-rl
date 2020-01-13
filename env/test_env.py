257f6303ecdee153928bc430d9fd2a20bba9dfd9
junjungoal <junyamada107@gmail.com>
2020-01-13 02:02:55 -0800

    add joints and collision in reacher

--- env/test_env.py
+++ env/test_env.py
@@ -4,7 +4,7 @@ import gym
 env = gym.make('reacher-obstacle-v0')
 env.reset()
 
-for i in range(100):
+for i in range(1000):
     env.render(mode='human')
     action = env.action_space.sample()
     env.step(action)
