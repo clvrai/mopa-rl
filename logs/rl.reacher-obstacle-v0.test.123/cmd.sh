908313bb9758ab8e0547440c9e713f503164661a
Jun yamada <junyamada107@gmail.com>
2020-01-14 17:40:53 +0900

    remove unnecessary files

--- logs/rl.reacher-obstacle-v0.test.123/cmd.sh
+++
@@ -1,6 +0,0 @@
-python -m rl --env reacher-obstacle-v0 --algo ppo --policy mlp --log_root_dir ./logs
-python -m rl --env reacher-obstacle-v0 --algo ppo --policy mlp --log_root_dir ./logs
-python -m rl.main --log_root_dir ./logs
-python -m rl.main --log_root_dir ./logs
-python -m rl.main --log_root_dir ./logs --wandb True
-python -m rl.main --log_root_dir ./logs --wandb True
