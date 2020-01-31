4bcba38cf1879736f4339747262c48f57da72cb8
Jun yamada <junyamada107@gmail.com>
2020-01-31 10:08:07 +0900

    change policy init

--- rl/policies/__init__.py
+++ rl/policies/__init__.py
@@ -1,12 +1,11 @@
 from .mlp_actor_critic import MlpActor, MlpCritic
-from .cnn_actor_critic import CNNActor, CNNCritic
 
 
 def get_actor_critic_by_name(name):
     if name == 'mlp':
         return MlpActor, MlpCritic
     elif name == 'cnn':
-        return CNNActor, CNNCritic
+        raise NotImplementedError()
     else:
         raise NotImplementedError()
 
