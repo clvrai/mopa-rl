5bbf9fbeece0bbecffc91ba5efff0fbeea466d35
Jun yamada <junyamada107@gmail.com>
2020-02-01 13:22:54 +0900

    change shell files

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -1,2 +1,2 @@
 # Baseline: SAC Non-HRL
-python -m rl.main --log_root_dir ./logs --wandb True --prefix baseline.sac_v5 --max_global_step 6000000
+python -m rl.main --log_root_dir ./logs --wandb True --prefix baseline.sac_vel.reacher --max_global_step 6000000 --env reacher-v0
