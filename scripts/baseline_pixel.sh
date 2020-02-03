d495e8985b1563ed92ef237b07f114f494bed574
junjungoal <junyamada107@gmail.com>
2020-02-03 15:30:05 -0800

    fix typo

--- scripts/baseline_pixel.sh
+++ scripts/baseline_pixel.sh
@@ -1,2 +1,2 @@
 # Baseline: SAC Non-HRL
-python -m rl.main --log_root_dir ./logs --prefix baseline.sac.pixel.v8 --max_global_step 40000000 --policy cnn --env reacher-pixel-v0 --gpu 0 --rl_hid_size 512 --max_grad_norm 0.5 --algo sac
+python -m rl.main --log_root_dir ./logs --prefix baseline.sac.pixel.v9 --max_global_step 40000000 --policy cnn --env reacher-pixel-v0 --gpu 0 --rl_hid_size 512 --max_grad_norm 0.5 --algo sac
