a39ba6e03cff5f114f0013d8a2686e8b79dcd906
junjungoal <junyamada107@gmail.com>
2020-02-09 23:56:57 -0800

    update scripts

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -1,2 +1,2 @@
 # Baseline: SAC Non-HRL
-python -m rl.main --log_root_dir ./logs --prefix baseline.td3.v8.smooth_target --max_global_step 6000000 --env reacher-v0 --gpu 0 --algo td3 --buffer_size 100000 --actor_update_freq 2 --rl_hid_size 512
+python -m rl.main --log_root_dir ./logs --prefix baseline.sac.512 --max_global_step 6000000 --env reacher-v0 --gpu 0 --buffer_size 100000 --actor_update_freq 2 --rl_hid_size 512
