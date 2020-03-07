7194ef4425984ea7f9b4ebbef76106898bb3f0ef
junjungoal <junyamada107@gmail.com>
2020-03-07 01:01:02 -0800

    add cart subgoal type

--- rl/rollouts.py
+++ rl/rollouts.py
@@ -112,9 +112,17 @@ class RolloutRunner(object):
                 joint_space = env.joint_space['default']
                 minimum = joint_space.low
                 maximum = joint_space.high
-                subgoal = curr_qpos[:env.model.nu]+meta_ac['subgoal']
+
+                if self._config.subgoal_type == 'joint':
+                    subgoal = curr_qpos[:env.model.nu]+meta_ac['subgoal']
+                else:
+                    subgoal_cart = meta_ac['subgoal']
+                    subgoal_cart = np.clip(subgoal_cart, meta_pi.ac_space['subgoal'].low, meta_pi.ac_space['subgoal'].high)
+                    ik_env._set_pos('subgoal', [subgoal_cart[0], subgoal_cart[1], self._env._get_pos('subgoal')[2]])
+                    result = qpos_from_site_pose_sampling(ik_env, 'fingertip', target_pos=ik_env._get_pos('subgoal'), target_quat=ik_env._get_quat('subgoal'),
+                                                          joint_names=env.model.joint_names[:env.model.nu], max_steps=100, trials=10, progress_thresh=10000.)
+                    subgoal = result.qpos[:env.model.nu].copy()
                 subgoal[env._is_jnt_limited] = np.clip(subgoal[env._is_jnt_limited], minimum[env._is_jnt_limited], maximum[env._is_jnt_limited])
-                #subgoal = np.clip(subgoal, minimum, maximum)
 
                 ik_env.set_state(np.concatenate([subgoal, env.sim.data.qpos[env.model.nu:]]), env.sim.data.qvel.ravel().copy())
                 goal_xpos, goal_xquat = self._get_mp_body_pos(ik_env, postfix='goal')
@@ -235,7 +243,15 @@ class RolloutRunner(object):
                 joint_space = env.joint_space['default']
                 minimum = joint_space.low
                 maximum = joint_space.high
-                subgoal = curr_qpos[:env.model.nu]+meta_ac['subgoal']
+                if self._config.subgoal_type == 'joint':
+                    subgoal = curr_qpos[:env.model.nu]+meta_ac['subgoal']
+                else:
+                    subgoal_cart = meta_ac['subgoal']
+                    subgoal_cart = np.clip(subgoal_cart, meta_pi.ac_space['subgoal'].low, meta_pi.ac_space['subgoal'].high)
+                    ik_env._set_pos('subgoal', [subgoal_cart[0], subgoal_cart[1], self._env._get_pos('subgoal')[2]])
+                    result = qpos_from_site_pose_sampling(ik_env, 'fingertip', target_pos=ik_env._get_pos('subgoal'), target_quat=ik_env._get_quat('subgoal'),
+                                                          joint_names=env.model.joint_names[:env.model.nu], max_steps=100, trials=10, progress_thresh=10000.)
+                    subgoal = result.qpos[:env.model.nu].copy()
                 subgoal[env._is_jnt_limited] = np.clip(subgoal[env._is_jnt_limited], minimum[env._is_jnt_limited], maximum[env._is_jnt_limited])
 
             ik_env.set_state(np.concatenate([subgoal, env.sim.data.qpos[env.model.nu:]]), env.sim.data.qvel.ravel().copy())
