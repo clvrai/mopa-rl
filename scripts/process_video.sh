cb60332741802dfa5f891cdf4b82c085415b1ceb
junjungoal <junyamada107@gmail.com>
2020-03-17 14:23:20 -0700

    remove unnecessary scripts

--- scripts/process_video.sh
+++
@@ -1,2 +0,0 @@
-ffmpeg -f concat -safe 0 -i <(for f in ./logs/rl.reacher-obstacle-v0.baseline_v5.123/video/*.mp4; do echo "file '$PWD/$f'"; done) -c copy ./logs/rl.reacher-obstacle-v0.baseline_v5.123/video/output.mp4
-ffmpeg -i ./logs/rl.reacher-obstacle-v0.baseline_v5.123/video/output.mp4 -vf "setpts=2*PTS" ./logs/rl.reacher-obstacle-v0.baseline_v5.123/video/output_slower.mp4
