af2e12c72390708b5eaa88a8438b6771c53b994a
junjungoal <junyamada107@gmail.com>
2020-04-22 18:10:13 -0700

    change obs

--- scripts/planner_subgoal_ppo_hrl.sh
+++ scripts/planner_subgoal_ppo_hrl.sh
@@ -17,7 +17,7 @@ elif [ $v = 3 ]
 then
     env='simple-mover-obstacle-v0'
     primitive_skills="reach_mp grasp manipulation_mp"
-    ignored_contact_geoms='None,None box,l_finger_g0/box,r_finger_g0/box,gripper_base_geom'
+    ignored_contact_geoms='None,None None,None box,l_finger_g0/box,r_finger_g0/box,gripper_base_geom'
 elif [ $v = 4 ]
 then
     env='simple-reacher-v0'
@@ -30,8 +30,8 @@ then
     ignored_contact_geoms='None,None'
 fi
 
-workers="1"
-prefix="4.20.PPO.INVALID_STATE.step5"
+workers="8"
+prefix="4.20.PPO.worker.8"
 #prefix="4.20.BASELINE.HRL"
 hrl="True"
 ll_type="mix"
@@ -45,12 +45,12 @@ rl_hid_size="256"
 meta_update_target="LL"
 meta_oracle="True"
 meta_subgoal_rew="0."
-max_meta_len="5"
+max_meta_len="10"
 buffer_size="12800"
-num_batches="10"
+num_batches="6"
 debug="False"
-rollout_length="4096"
-batch_size="256"
+rollout_length="512"
+batch_size="32"
 evaluate_interval='10'
 ckpt_interval='10'
 reward_type="dense"
@@ -70,10 +70,10 @@ algo='ppo'
 group='4.20.PPO'
 rl_activation='tanh'
 subgoal_hindsight="True"
+env_debug='False'
 # max_grad_norm='0.5'
 
-#mpiexec -n $workers
-python -m rl.main \
+mpiexec -n $workers python -m rl.main \
     --log_root_dir $log_root_dir \
     --wandb True \
     --prefix $prefix \
@@ -116,4 +116,5 @@ python -m rl.main \
     --group $group \
     --rl_activation $rl_activation \
     --subgoal_hindsight $subgoal_hindsight \
+    --env_debug $env_debug
     # --max_grad_norm $max_grad_norm
