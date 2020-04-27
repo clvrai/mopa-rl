748d513c3750b3c75a2a5e94a453776521870b74
junjungoal <junyamada107@gmail.com>
2020-04-26 22:28:30 -0700

    delete unnecessary file

--- rl/trainer.py
+++ rl/trainer.py
@@ -131,10 +131,6 @@ class Trainer(object):
                 )
             else:
                 from rl.low_level_ppo_agent import LowLevelPPOAgent
-                # from rl.low_level_ppo_single_critic_agent import LowLevelPPOSingleCriticAgent
-                # self._agent = LowLevelPPOSingleCriticAgent(
-                #     config, ll_ob_space, ac_space, subgoal_space, actor, critic, non_limited_idx
-                # )
                 self._agent = LowLevelPPOAgent(
                     config, ll_ob_space, ac_space, subgoal_space, actor, critic, non_limited_idx
                 )
