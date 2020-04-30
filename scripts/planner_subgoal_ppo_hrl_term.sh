beef94c58504774c3c1670a8e173d94601620f7a
junjungoal <junyamada107@gmail.com>
2020-04-30 03:09:35 -0700

    change script

--- scripts/planner_subgoal_ppo_hrl_term.sh
+++ scripts/planner_subgoal_ppo_hrl_term.sh
@@ -32,7 +32,7 @@ then
 fi
 
 workers="8"
-prefix="4.29.TERMINATION.TTHRESHOLD.0.0.debug"
+prefix="4.29.TERMINATION.TTHRESHOLD.0.0.clip.target_qpos"
 #prefix="4.20.BASELINE.HRL"
 hrl="True"
 ll_type="mix"
@@ -49,7 +49,7 @@ meta_subgoal_rew="0."
 max_meta_len="10"
 buffer_size="12800"
 num_batches="10"
-debug="True"
+debug="False"
 rollout_length="1024"
 batch_size="64"
 evaluate_interval='5'
@@ -76,8 +76,7 @@ termination='True'
 group='SkillOrdering.Termination'
 # max_grad_norm='0.5'
 
-#mpiexec -n $workers
-python -m rl.main \
+mpiexec -n $workers python -m rl.main \
     --log_root_dir $log_root_dir \
     --wandb True \
     --prefix $prefix \
