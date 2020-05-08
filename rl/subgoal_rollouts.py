591779ea2ec6fbdc7d48cb128c09cc3bee72e7f6
junjungoal <junyamada107@gmail.com>
2020-05-08 15:58:12 -0700

    modify info

--- rl/subgoal_rollouts.py
+++ rl/subgoal_rollouts.py
@@ -192,7 +192,7 @@ class SubgoalRolloutRunner(object):
                         # reward, _  = env.compute_reward(np.zeros(env.sim.model.nu))
                         reward = self._config.invalid_planner_rew
                         rollout.add({'ob': ll_ob, 'meta_ac': meta_ac, 'ac': subgoal_ac, 'ac_before_activation': ac_before_activation})
-                        done, info, _ = env._after_step(reward, False, info)
+                        done, info, _ = env._after_step(reward, False, {})
                         rollout.add({'done': done, 'rew': reward})
                         ep_len += 1
                         step += 1
