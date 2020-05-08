3f3f377ddf86db5067c5f94bc93e67e2afee46cd
junjungoal <junyamada107@gmail.com>
2020-05-08 15:52:41 -0700

    add pusher hard

--- env/pusher/simple_pusher_obstacle_hard.py
+++ env/pusher/simple_pusher_obstacle_hard.py
@@ -43,8 +43,8 @@ class SimplePusherObstacleHardEnv(BaseEnv):
         self._stages = [False] * self._num_primitives
         self._stage = 0
         while True:
-            goal = np.random.uniform(low=[-0.2, 0.1], high=[0., 0.2], size=2)
-            box = np.random.uniform(low=[-0.2, 0.1], high=[0., 0.2], size=2)
+            goal = np.random.uniform(low=-0.2, high=0.2, size=2)
+            box = np.random.uniform(low=-0.2, high=0.2, size=2)
             qpos = np.random.uniform(low=-0.1, high=0.1, size=self.sim.model.nq) + self.sim.data.qpos.ravel()
             qpos[-4:-2] = goal
             qpos[-2:] = box
@@ -52,7 +52,7 @@ class SimplePusherObstacleHardEnv(BaseEnv):
             qvel[-4:-2] = 0
             qvel[-2:] = 0
             self.set_state(qpos, qvel)
-            if self.sim.data.ncon == 0 and self._get_distance('box', 'target') > 0.1:
+            if self.sim.data.ncon == 0 and self._get_distance('box', 'target') > 0.1 and np.linalg.norm(goal) > 0.1:
                 self.goal = goal
                 self.box = box
                 break
