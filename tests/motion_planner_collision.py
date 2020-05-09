3cb82d44b568f7640ba7dbbe9304b0b9b49e0797
junjungoal <junyamada107@gmail.com>
2020-05-08 22:51:38 -0700

    change action scale

--- tests/motion_planner_collision.py
+++ tests/motion_planner_collision.py
@@ -12,7 +12,8 @@ parser = argparser()
 add_arguments(parser)
 args, unparsed = parser.parse_known_args()
 
-env = gym.make('simple-pusher-obstacle-v0', **args.__dict__)
+args.env = 'simple-pusher-obstacle-v0'
+env = gym.make(args.env, **args.__dict__)
 
 passive_joint_idx = []
 ignored_contacts = []
