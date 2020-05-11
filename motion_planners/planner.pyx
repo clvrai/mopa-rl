aabba51fdd2352d674fa387c8ba89b10ba276496
junjungoal <junyamada107@gmail.com>
2020-05-10 21:19:04 -0700

    resolve conflict

--- motion_planners/planner.pyx
+++ motion_planners/planner.pyx
@@ -8,7 +8,7 @@ from libcpp.pair cimport pair
 
 cdef extern from "KinematicPlanner.h" namespace "MotionPlanner":
   cdef cppclass KinematicPlanner:
-        KinematicPlanner(string, string, int, double, double, string, double, double, double, vector[int], vector[string], vector[pair[int, int]]) except +
+        KinematicPlanner(string, string, int, double, double, string, double, double, double, vector[int], vector[string], vector[pair[int, int]], double) except +
         string xml_filename
         string opt
         int num_actions
@@ -22,14 +22,15 @@ cdef extern from "KinematicPlanner.h" namespace "MotionPlanner":
         vector[int] passive_joint_idx
         vector[string] glue_bodies
         vector[pair[int, int]] ignored_contacts
+        double contact_threshold
 
         vector[vector[double]] plan(vector[double], vector[double], double, double)
         void removeCollision(int, int, int)
 
 cdef class PyKinematicPlanner:
     cdef KinematicPlanner *thisptr
-    def __cinit__(self, string xml_filename, string algo, int num_actions, double sst_selection_radius, double sst_pruning_radius, string opt, double threshold, double _range, double constructTime, vector[int] passive_joint_idx, vector[string] glue_bodies, vector[pair[int, int]] ignored_contacts):
-        self.thisptr = new KinematicPlanner(xml_filename, algo, num_actions, sst_selection_radius, sst_pruning_radius, opt, threshold, _range, constructTime, passive_joint_idx, glue_bodies, ignored_contacts)
+    def __cinit__(self, string xml_filename, string algo, int num_actions, double sst_selection_radius, double sst_pruning_radius, string opt, double threshold, double _range, double constructTime, vector[int] passive_joint_idx, vector[string] glue_bodies, vector[pair[int, int]] ignored_contacts, double contact_threshold):
+        self.thisptr = new KinematicPlanner(xml_filename, algo, num_actions, sst_selection_radius, sst_pruning_radius, opt, threshold, _range, constructTime, passive_joint_idx, glue_bodies, ignored_contacts, contact_threshold)
 
     def __dealloc__(self):
         del self.thisptr
