ac2dfb28dea47ba68fad1ac097c1a37b33b74919
junjungoal <junyamada107@gmail.com>
2020-05-11 17:57:15 -0700

    fix typo

--- rl/sac_agent.py
+++ rl/sac_agent.py
@@ -57,7 +57,7 @@ class SACAgent(BaseAgent):
         self._planner = None
         if config.planner_integration:
             self._planner = PlannerAgent(config,ac_space, non_limited_idx, config.passive_joint_idx, config.ignored_contact_geom_ids[0])
-            self._simple_planner = PlanenrAgent(config, ac_space, non_limited_idx, config.passive_joint_idx, config.ignored_contact_geom_ids[0], goal_bias=1.0)
+            self._simple_planner = PlannerAgent(config, ac_space, non_limited_idx, config.passive_joint_idx, config.ignored_contact_geom_ids[0], goal_bias=1.0)
             self._ac_rl_minimum = config.ac_rl_minimum
             self._ac_rl_maximum = config.ac_rl_maximum
 
