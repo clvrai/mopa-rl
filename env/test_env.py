06a25b679ecca45f852737fdc58578f0fd2adf40
gautams3 <gautams3@users.noreply.github.com>
2020-07-04 14:06:25 -0700

    Rendering workaround: test_env can save to video

--- env/test_env.py
+++ env/test_env.py
@@ -3,6 +3,10 @@ import gym
 from config import argparser
 import numpy as np
 import time
+import os
+import cv2
+from util.misc import save_video, render_frame, mujocopy_render_hack
+np.set_printoptions(precision=3)
 
 # mujocopy_render_hack() # rendering fix for gautam
 is_save_video = False
@@ -21,7 +25,6 @@ elif 'sawyer' in args.env:
     from config.sawyer import add_arguments
 elif 'jaco' in args.env:
     from config.jaco import add_arguments
-
 else:
     raise ValueError('args.env (%s) is not supported' % args.env)
 
@@ -34,14 +37,28 @@ obs = env.reset()
 obs = env.reset()
 # env.render('human') # uncomment if you don't use mujocopy hack
 
-#for i in range(10000):
-timestep = 0
+frames = []
+frames.append([render_frame(env, 0)])
 env.reset_visualized_indicator()
-while True:
+for i in range(100):
+# while True:
     # env.render(mode='rgb_array')
     action = env.action_space.sample()
     obs, reward, done, _ = env.step(action)
-    env.render(mode='human')
+    if is_save_video:
+        # WARNING: don't is_save_video with a 'while True` loop since it runs forever
+        frames[0].append(render_frame(env, i))
+    else:
+        env.render(mode='human')
+
     if done:
         print('done')
         break
+
+if is_save_video:
+    prefix_path = './tmp/motion_planning_test/'
+    if not os.path.exists(prefix_path):
+        os.makedirs(prefix_path)
+    # for i, episode_frames in enumerate(frames):
+    fpath = os.path.join(prefix_path, 'test_trial.mp4')
+    save_video(fpath, frames[0], fps=5)
