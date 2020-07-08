fd78c2ce394ebb6f4fe7f4e5ba2e5c3921486e0f
junjungoal <junyamada107@gmail.com>
2020-07-08 15:44:08 -0700

    remove unnecessary files

--- scripts/test_env.sh
+++
@@ -1,5 +0,0 @@
-python -m tests.test_env --env simple-mover-v0
-#python -m env.test_env --env sawyer-reach-push-pick-place-wall-v0
-#python -m env.test_env --env sawyer-move-v0
-#python -m env.test_env --env sawyer-pick-place-v0
-#--camera_name birdview
