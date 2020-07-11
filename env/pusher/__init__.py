aef54944393d4b9cdf97bbe6f0aa3a00cae3680b
junjungoal <junyamada107@gmail.com>
2020-07-11 03:32:39 -0700

    add new env

--- env/pusher/__init__.py
+++ env/pusher/__init__.py
@@ -3,6 +3,7 @@ from env.pusher.pusher import PusherEnv
 from env.pusher.simple_pusher_pixel import SimplePusherPixelEnv
 from env.pusher.simple_pusher_obstacle import SimplePusherObstacleEnv
 from env.pusher.simple_pusher_obstacle_hard import SimplePusherObstacleHardEnv
+from env.pusher.simple_pusher_obstacle_hard_v1 import SimplePusherObstacleHardV1Env
 from env.pusher.pusher_obstacle import PusherObstacleEnv
 from env.pusher.simple_pusher_obstacle_pixel import SimplePusherObstaclePixelEnv
 from env.pusher.primitives.pusher_push import PusherPushEnv
