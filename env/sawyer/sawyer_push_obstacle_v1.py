3c963cecadaba6e5cd9963b7371062f59e366a54
junjungoal <junyamada107@gmail.com>
2020-07-14 07:20:45 -0700

    change reward func

--- env/sawyer/sawyer_push_obstacle_v1.py
+++ env/sawyer/sawyer_push_obstacle_v1.py
@@ -75,11 +75,11 @@ class SawyerPushObstacleV1Env(SawyerEnv):
             cube_to_target = np.linalg.norm(cube_pos[:2]-target_pos[:2])
             reward_push = 0.
             reward_reach = 0.
-            if gripper_to_cube < 0.3 and gripper_site_pos[2] > 0.8:
-                reward_reach += 0.1*(1-np.tanh(2*gripper_to_cube))
+            if gripper_to_cube < 0.2 and gripper_site_pos[2] > 0.8:
+                reward_reach += 0.1*(1-np.tanh(5*gripper_to_cube))
 
             if cube_to_target < 0.3:
-                reward_push += 0.3*(1-np.tanh(2*cube_to_target))
+                reward_push += 0.3*(1-np.tanh(5*cube_to_target))
             reward += reward_push + reward_reach
             info = dict(reward_reach=reward_reach, reward_push=reward_push)
 
