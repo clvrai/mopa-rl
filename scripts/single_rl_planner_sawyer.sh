b5f24d2f71ceec4d8330d64fc080e3605a43d230
junjungoal <junyamada107@gmail.com>
2020-07-13 17:01:49 -0700

    update script

--- scripts/single_rl_planner_sawyer.sh
+++ scripts/single_rl_planner_sawyer.sh
@@ -1,6 +1,6 @@
 #<!/bin/bash -x
 
-prefix="SAC.PLANNER.AUGMENTED.piecewise0.7.ac_range0.5.scale0.2.reuse.sparse.v2"
+prefix="SAC.PLANNER.AUGMENTED.piecewise0.7.ac_range1.0.reuse.sparse.v9"
 gpu=$1
 seed=$2
 algo='sac'
@@ -20,7 +20,7 @@ planner_integration="True"
 allow_manipulation_collision="True"
 alpha="1.0"
 reuse_data_type="random"
-action_range="0.5"
+action_range="1.0"
 ac_rl_minimum="-0.7"
 ac_rl_maximum="0.7"
 invalid_planner_rew="-0.0"
