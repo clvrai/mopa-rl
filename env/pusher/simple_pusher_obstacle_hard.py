fa6f0da90fbd8b59b0462c8d785b3200ebb51633
junjungoal <junyamada107@gmail.com>
2020-07-15 16:28:29 -0700

    add geom

--- env/pusher/simple_pusher_obstacle_hard.py
+++ env/pusher/simple_pusher_obstacle_hard.py
@@ -107,7 +107,7 @@ class SimplePusherObstacleHardEnv(BaseEnv):
 
     @property
     def static_geoms(self):
-        return ['obstacle1_geom', 'obstacle2_geom', 'obstacle3_geom', 'obstacle4_geom']
+        return ['obstacle1_geom', 'obstacle2_geom', 'obstacle3_geom', 'obstacle4_geom', 'obstaacle5_geom']
 
     @property
     def static_geom_ids(self):
