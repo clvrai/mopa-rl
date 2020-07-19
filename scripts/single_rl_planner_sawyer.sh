fff91ac05d2173abea5794c0988967b0a19bf16a
junjungoal <junyamada107@gmail.com>
2020-07-19 15:36:58 -0700

    modify env and update script

--- scripts/single_rl_planner_sawyer.sh
+++ scripts/single_rl_planner_sawyer.sh
@@ -1,14 +1,14 @@
 #<!/bin/bash -x
 
-prefix="SAC.PLANNER.AUGMENTED.piecewise0.9.ac_range1.0.scale0.5.reuse5.sparse.no_manipu"
+prefix="SAC.PLANNER.AUGMENTED.piecewise0.7.ac_range1.0.scale0.2"
 gpu=$1
 seed=$2
 algo='sac'
 rl_activation="relu"
 num_batches="1"
 log_interval="1000"
-env="sawyer-push-obstacle-v1"
-max_episode_step="400"
+env="sawyer-push-obstacle-v2"
+max_episode_step="250"
 debug="False"
 batch_size="256"
 reward_type='sparse'
@@ -20,9 +20,9 @@ planner_integration="True"
 allow_manipulation_collision="False"
 alpha="1.0"
 reuse_data_type="random"
-action_range="1.0"
-ac_rl_minimum="-0.9"
-ac_rl_maximum="0.9"
+action_range="0.5"
+ac_rl_minimum="-0.7"
+ac_rl_maximum="0.7"
 invalid_planner_rew="-0.0"
 extended_action="False"
 stochastic_eval="True"
@@ -39,9 +39,9 @@ step_size="0.02"
 success_reward="150.0"
 add_curr_rew="True"
 discount_factor='0.99'
-max_reuse_data='5'
+max_reuse_data='15'
 min_reuse_span='20'
-reward_scale="0.5"
+reward_scale="0.2"
 
 python -m rl.main \
     --log_root_dir $log_root_dir \
